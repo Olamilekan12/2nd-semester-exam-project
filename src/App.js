682c2be0dcaa91c8e2ff0a40bc418eef61bd3a2f
Olamilekan12 <olamilekanogunbade3@gmail.com>
2024-04-23 10:42:35 -0700

    Fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RepositoryList from "./RepositoryList";
 import RepositoryDetail from "./RepositoryDetail";
 import ErrorBoundary from "./ErrorBoundary";
 import NotFound from "./NotFound";
 import CreateRepositoryModal from "./CreateRepositoryModal";
-
 
 const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -21,18 +20,18 @@ const App = () => {
   return (
     <ErrorBoundary>
       <Router>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={(props) => <RepositoryList {...props} />}
-          />
-          <Route
-            path="/repo/:id"
-            render={(props) => <RepositoryDetail {...props} />}
-          />
-          <Route component={NotFound} /> {/* Route for 404 page */}
-        </Switch>
+        <Routes>
+        <Route
+          exact
+          path="/"
+          render={(props) => <RepositoryList {...props} />}
+        />
+        <Route
+          path="/repo/:id"
+          render={(props) => <RepositoryDetail {...props} />}
+        />
+        <Route component={NotFound} /> {/* Route for 404 page */}
+        </Routes>
       </Router>
       <CreateRepositoryModal isOpen={isModalOpen} onClose={closeModal} />
       <button onClick={openModal}>Create New Repository</button>
